27024febe5b5af2c9f29bd5a07a9787a0c765857
Viet Nguyen <vietnguyengit@outlook.com>
2021-09-09 15:35:53 +1000

    multi components

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Counters from './components/counters';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
