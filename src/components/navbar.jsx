ee4938a0d0270a32eb281e71d060b744e9ff4f0e
Viet Nguyen <vietnguyengit@outlook.com>
2021-09-09 17:39:58 +1000

    state less function

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    state = {}
-
-    render() {
-        return (
-            <nav class="navbar navbar-light bg-light">
-                <a class="navbar-brand" href="#">
-                    Navbar <span className="badge badge-pill badge-primary">{this.props.totalCounters}</span>
-                </a>
-            </nav>
-        );
-    }
+//state less function
+const NavBar = (props) => {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                Navbar <span className="badge badge-pill badge-primary">{props.totalCounters}</span>
+            </a>
+        </nav>
+    );
 }
 
 export default NavBar;
